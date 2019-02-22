2d2597f26a3c600ae18e119a19bd5e746ae3a5fc
roberto <roberto.forlani@gmail.com>
2019-02-22 13:50:59 +0100

    1.2.2

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
-from distutils.core import setup
+
+
+with open("README.md", "r") as fh:
+      long_description = fh.read()
 
 setup(name='PyAsynch',
-      version='1.2',
+      version='1.2.2',
       description='Python asynch distributed and scalable Queue Services',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       author='Roberto Forlani',
       author_email='roberto.forlani@gmail.com',
-      url='https://www.python.org/sigs/distutils-sig/',
+      url='https://github.com/hoploop/pyasynch',
+      install_requires=["pika","python-dateutil","tornado"],
       packages=find_packages()
       )
